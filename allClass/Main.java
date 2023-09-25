dfa7bc0ff13dbe0117ba58eda12551fa704b805d
BrnGomes0 <brunownog05@gmail.com>
2023-09-25 16:07:03 -0300

    Improved the logic
    Improve the code OOP

--- allClass/Main.java
+++ allClass/Main.java
@@ -6,7 +6,7 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class Main{
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         Scanner scan = new Scanner(System.in);
         Locale.setDefault(Locale.US);
@@ -20,75 +20,95 @@ public class Main{
         boolean positionTrain = true;
         boolean velocityTrain = true;
 
+
         // Variables about train
         double valuePositionA = 0;
         double valueVelocityA = 0;
         double valuePositionB = 0;
         double valueVelocityB = 0;
-        double finalPositionA, finalPositionB, time;
+        double finalPositionA, finalPositionB, time, hourCollision;
         final double hour = 17;
 
 
         // Validation Train velocity
         while (true) {
             while (velocityTrain) {
-                System.out.println("Insert Velocity train A: ");
-                valueVelocityA = scan.nextDouble();
-                if (valueVelocityA < 0 || valueVelocityA > 300) {
-                    System.out.println("The minimum speed is 0 and the maximum is 300Km/h!");
+                // Validation of Velocity Train A
+                try{
+                    System.out.println("Insert Velocity train A: ");
+                    valueVelocityA = scan.nextDouble();
+                    if (valueVelocityA < 0 || valueVelocityA > 300) {
+                        throw new Exception("Invalid Input!");
+                    }
+                }catch (Exception e){
+                    System.out.println("Invalid Input! The max velocity is 300Km/h!");
                     continue;
                 }
                 trainOne.setvalueVelocityTrain(valueVelocityA);
 
-                System.out.println("Insert Velocity train B: "); // Improve the code in While back in the wrong input user
-                valueVelocityB = scan.nextDouble();
-                if (valueVelocityB > 0 || valueVelocityB < -300) {
-                    System.out.println("The velocity train B, need be negative!");
+                // Validation of Velocity Train B
+                try {
+                    System.out.println("Insert Velocity train B: "); // Improve the code in While back in the wrong input user
+                    valueVelocityB = scan.nextDouble();
+                    if (valueVelocityB > 0 || valueVelocityB < -300) {
+                        throw new Exception("Invalid Input!");
+                    }
+                }catch (Exception e){
+                    System.out.println("Invalid Input! The minimum velocity is 0 and the max is -300Km/h!");
                     continue;
                 }
                 trainSecond.setvalueVelocityTrain(valueVelocityB);
                 velocityTrain = false;
-
             }
             // Validation Train position
             while (positionTrain) {
-                System.out.println("Insert the position train A: ");
-                valuePositionA = scan.nextDouble();
-                if (valuePositionA < 0 || valuePositionA > 10_000) {
-                    System.out.println("The minimum position is Km0 and the maximum is Km10.000!");
+                try {
+                    System.out.println("Insert the position train A: ");
+                    valuePositionA = scan.nextDouble();
+                    if (valuePositionA < 0 || valuePositionA > 10_000) {
+                        throw new Exception("Invalid Input");
+                    }
+                }catch (Exception e){
+                    System.out.println("Invalid Input!The max position is Km10.000 and the minimum is 0!");
                     continue;
                 }
                 trainOne.setValuePositionTrain(valuePositionA);
 
-                System.out.println("Insert the position train B: ");
-                valuePositionB = scan.nextDouble();
-                if (valuePositionB < 0 || valuePositionB > 10_000) {
-                    System.out.println("The minimum position is Km0 and the maximum is Km10.000!");
-                    continue;
+                try{
+                    System.out.println("Insert the position train B: ");
+                    valuePositionB = scan.nextDouble();
+                    if (valuePositionB < 0 || valuePositionB > 10_000) {
+                        throw new Exception("Invalid Input!");
+                    }
+                }catch (Exception e){
+                    System.out.println("Invalid Input!Invalid Input!The max position is Km10.000 and the minimum is 0!");
                 }
                 trainSecond.setValuePositionTrain(valuePositionB);
                 positionTrain = false;
             }
-            // Logic Train Collision -> S = So + v*t (Space) and Time (t = Soa - Sob / Va - Vb)
-            // The time
+            // Logic Train Collision
+
+            // Calculate the time of collision
             time = (trainOne.getValuePositionTrain() - trainSecond.getValuePositionTrain()) / (trainSecond.getValueVelocityTrain() - trainOne.getValueVelocityTrain());
-            System.out.printf("The time is %.1f seconds\n", time * 60 * 60);
+            double timeSeconds = time * 60 * 60;
 
-            // The KM Train A
+            // The KM Train A - Final position
             finalPositionA = trainOne.getValuePositionTrain() + trainOne.getValueVelocityTrain() * time;
-            System.out.printf("The position train KM%.1f\n", finalPositionA);
-
-            // The KM Train B
+            // The KM Train B - Final position
             finalPositionB = trainSecond.getValuePositionTrain() + trainSecond.getValueVelocityTrain() * time;
-            System.out.printf("The position train KM%.1f\n", finalPositionB);
-
-            // Logic for show the time correct
-            double hourCorret;
-            
 
 
-            break;
+            System.out.printf("The collision of trains will happen in KM %.1f e will occur after %.0f seconds.\n", finalPositionA, timeSeconds);
+
+            System.out.println("Try Again? [y/n]");
+            scan.nextLine();
+            String answerUser = scan.nextLine();
+            if(answerUser.equalsIgnoreCase("y")){
+                velocityTrain = true;
+                positionTrain = true;
+            } else if (answerUser.equalsIgnoreCase("n")) {
+                break;
+            }
         }
-
     }
 }
